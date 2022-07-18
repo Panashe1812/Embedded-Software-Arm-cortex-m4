305b16884591549b9f2d7eef7a963910f03b49fd
Panashe Chisasa <30976197+Panashe1812@users.noreply.github.com>
2022-07-18 08:55:00 +0100

    Add files via upload
    Arduino projects

---
+++ I2C_tutorial/I2C_tutorial.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+
+int temp_adrs= 72;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void loop() {
+  //Send a request
+  //start talking
+  Wire.beginTransmission(temp_adrs);
+  // ask for register 0
+  Wire.write(0);
+  //complete transmission
+  Wire.endTransmission();
+  //request i byte
+  Wire.requestFrom(temp_adrs,1);
+  // wait for response
+  while(Wire.available()==0);
+  int c =Wire.read();
+  
+
+}
---
+++ red_fade/red_fade.ino
@@ -0,0 +1,27 @@
+# define red 6
+# define blue 5
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(red,OUTPUT);
+pinMode(blue,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int count=0;count<255;count++)
+{   
+    analogWrite(red,count);
+    analogWrite(blue,255-count);
+    delay(50);
+    /*digitalWrite(blue,LOW);
+    digitalWrite(red,HIGH);
+    delay(1000);
+    digitalWrite(blue,HIGH);
+    digitalWrite(red,LOW);
+    delay(2000);*/
+    
+}
+
+}
